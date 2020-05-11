06b47c3dfbd3b39b59b4a1ada34381e0c5627452
Sharif Uddin <shorif2000@gmail.com>
2020-05-11 11:16:43 +0000

    add new url

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 8887
 
+var xml = require('xml');
+
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 
@@ -17,8 +19,18 @@ server.get('/', (request, response) => {
  }
 })
 
-server.get('/nitx/jsp/xquery.jsp', (request, response) => {
- if (request.method === 'GET') {
-   response.status(200).jsonp({})
- }
+server.post('/nitx/jsp/xquery.jsp', (request, response) => {
+ //if (request.method === 'GET') {
+	 response.set('Content-Type', 'text/xml');
+  response.send(xml({}));
+
+   //response.status(200).jsonp({})
+ //}
 })
+
+server.get('/nitx/api/1/topology/flowcheck/bulk', (request, response) => {
+         response.set('Content-Type', 'text/json');
+  response.send({});
+
+})
+
